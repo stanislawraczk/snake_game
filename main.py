5d790b632d83ffb54a9f4fc4c3e4dd98f41d5d7b
Stanislaw <raczkiewicz.stas@gmail.com>
2021-08-08 12:25:04 +0200

    Cleaned the code

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ GREEN = (0, 128, 0)
 RED = (255, 0, 0)
 
 
-class GameLogic:
+class Game:
     def __init__(self):
         self.message = 'Press Enter to start or Escape to quit'
         self.running = False
@@ -39,13 +39,76 @@ class GameLogic:
         self.running = False
         self.menu_running = True
 
+    def menu_loop(self, menu_text_in, screen_in):
+        for event in pygame.event.get():
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE:
+                    self.menu_running = False
+                    self.running = False
+                if event.key == K_RETURN:
+                    self.menu_running = False
+                    self.running = True
+            if event.type == QUIT:
+                self.menu_running = False
+                self.running = False
+        menu_text_surf = menu_text_in.render(self.message, False, WHITE)
+        screen_in.blit(menu_text_surf, (0, 0))
+        pygame.display.flip()
+
+    def game_loop(self, score_in, screen_in, snake_in, tails_in, fruit_in):
+        while self.running:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        self.running = False
+                        self.menu_running = True
+                    if event.key in (K_w, K_s, K_a, K_d):
+                        snake_in.set_direction(event.key)
+                        Tail.follow(tails_in)
+                        snake_in.update()
+                        snake_in.moved = True
+                if event.type == QUIT:
+                    self.running = False
+
+            if not snake_in.moved:
+                Tail.follow(tails_in)
+                snake_in.update()
+            snake_in.moved = False
+            snake_in.check_collision(tails_in)
+            if snake_in.collision:
+                self.game_over()
+                score_in -= 10
+
+            if not game.fruit_on_screen:
+                fruit_x = SNAKE_SIZE * random.randint(1, (SCREEN_WIDTH // SNAKE_SIZE) - 1)
+                fruit_y = SNAKE_SIZE * random.randint(1, (SCREEN_HEIGHT // SNAKE_SIZE) - 1)
+                fruit_in = Fruit(screen_x=fruit_x, screen_y=fruit_y)
+                self.fruit_on_screen = True
+
+            if snake_in.check_fruit(fruit_in):
+                score_in += 10
+                self.fruit_on_screen = False
+                tails_in.append(Tail(screen_x=2 * snake_in.rect.centerx, screen_y=2 * snake_in.rect.centery))
+
+            print(score_in)
+
+            screen_in.fill(BLACK)
+            screen_in.blit(snake_in.surf, snake_in.rect)
+            screen_in.blit(fruit_in.surf, fruit_in.rect)
+            for tail in tails_in:
+                screen_in.blit(tail.surf, tail.rect)
+
+            pygame.display.flip()
+
+            pygame.time.wait(200)
+
 
 class Snake(pygame.sprite.Sprite):
-    def __init__(self, width=SNAKE_SIZE, height=SNAKE_SIZE, x=SCREEN_WIDTH, y=SCREEN_HEIGHT):
+    def __init__(self, width=SNAKE_SIZE, height=SNAKE_SIZE, screen_x=SCREEN_WIDTH, screen_y=SCREEN_HEIGHT):
         super().__init__()
         self.surf = pygame.Surface((width, height))
         self.surf.fill(GREEN)
-        self.rect = self.surf.get_rect(center=(x / 2, y / 2))
+        self.rect = self.surf.get_rect(center=(screen_x / 2, screen_y / 2))
         self.direction = ''
         self.collision = False
         self.moved = False
@@ -95,8 +158,8 @@ class Snake(pygame.sprite.Sprite):
 
 
 class Tail(Snake):
-    def __init__(self, x=0, y=0):
-        super(Tail, self).__init__(width=SNAKE_SIZE, height=SNAKE_SIZE, x=x, y=y)
+    def __init__(self, screen_x=0, screen_y=0):
+        super(Tail, self).__init__(width=SNAKE_SIZE, height=SNAKE_SIZE, screen_x=screen_x, screen_y=screen_y)
 
     @staticmethod
     def follow(tails_in):
@@ -108,83 +171,23 @@ class Tail(Snake):
 
 
 class Fruit(pygame.sprite.Sprite):
-    def __init__(self, width=SNAKE_SIZE, height=SNAKE_SIZE, x=SCREEN_WIDTH, y=SCREEN_HEIGHT):
+    def __init__(self, width=SNAKE_SIZE, height=SNAKE_SIZE, screen_x=SCREEN_WIDTH, screen_y=SCREEN_HEIGHT):
         super(Fruit, self).__init__()
         self.surf = pygame.Surface((width, height))
         self.surf.fill(RED)
-        self.rect = self.surf.get_rect(center=(x, y))
+        self.rect = self.surf.get_rect(center=(screen_x, screen_y))
 
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
-tails = list()
-
 menu_text = pygame.font.SysFont('Calibri', 30)
+game = Game()
 snake = Snake()
-game = GameLogic()
-snake.direction = 'UP'
-x = 30 * random.randint(1, (SCREEN_WIDTH // 30) - 1)
-y = 30 * random.randint(1, (SCREEN_HEIGHT // 30) - 1)
-fruit = Fruit(x=x, y=y)
+tails = list()
 score = 0
+x = SNAKE_SIZE * random.randint(1, (SCREEN_WIDTH // SNAKE_SIZE) - 1)
+y = SNAKE_SIZE * random.randint(1, (SCREEN_HEIGHT // SNAKE_SIZE) - 1)
+fruit = Fruit(screen_x=x, screen_y=y)
 
 while game.menu_running:
-    for event in pygame.event.get():
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                game.menu_running = False
-                game.running = False
-            if event.key == K_RETURN:
-                game.menu_running = False
-                game.running = True
-        if event.type == QUIT:
-            game.menu_running = False
-            game.running = False
-    menu_text_surf = menu_text.render(game.message, False, WHITE)
-    screen.blit(menu_text_surf, (0, 0))
-    pygame.display.flip()
-
-    while game.running:
-        for event in pygame.event.get():
-            if event.type == KEYDOWN:
-                if event.key == K_ESCAPE:
-                    game.running = False
-                    game.menu_running = True
-                if event.key in (K_w, K_s, K_a, K_d):
-                    snake.set_direction(event.key)
-                    Tail.follow(tails)
-                    snake.update()
-                    snake.moved = True
-            if event.type == QUIT:
-                game.running = False
-
-        if not snake.moved:
-            Tail.follow(tails)
-            snake.update()
-        snake.moved = False
-        snake.check_collision(tails)
-        if snake.collision:
-            game.game_over()
-
-        if not game.fruit_on_screen:
-            x = 30 * random.randint(1, (SCREEN_WIDTH // 30) - 1)
-            y = 30 * random.randint(1, (SCREEN_HEIGHT // 30) - 1)
-            fruit = Fruit(x=x, y=y)
-            game.fruit_on_screen = True
-
-        if snake.check_fruit(fruit):
-            score += 10
-            game.fruit_on_screen = False
-            tails.append(Tail(x=2*snake.rect.centerx, y=2*snake.rect.centery))
-
-        print(score)
-
-        screen.fill(BLACK)
-        screen.blit(snake.surf, snake.rect)
-        screen.blit(fruit.surf, fruit.rect)
-        for tail in tails:
-            screen.blit(tail.surf, tail.rect)
-
-        pygame.display.flip()
-
-        pygame.time.wait(500)
+    game.menu_loop(menu_text, screen)
+    game.game_loop(score_in=score, screen_in=screen, snake_in=snake, tails_in=tails, fruit_in=fruit)
